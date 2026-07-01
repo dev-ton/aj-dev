fc0969dca8fc74a881f6aa3d65bfa4dc59bd83bb
Antonin Jech <a.jech@audaris.de>
2026-07-01 20:29:11 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dev-ton.github.io",
-  base: "/aj-dev",
+  site: "https://dev-ton.github.io/antonin-jech",
+  base: "/antonin-jech",
   vite: {
     plugins: [tailwindcss()],
   },
